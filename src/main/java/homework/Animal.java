6701a09299aee0a392f8987b08462b698a18a682
Diana Matjuseva <ptr679@gmail.com>
2022-02-18 19:48:37 +0200

    [feature/create_readme]: Homework_Animal(corrected version)

--- src/main/java/homework/Animal.java
+++ src/main/java/homework/Animal.java
@@ -1,73 +1,149 @@
-package homework;
+ package homework;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+
+
+ @Slf4j
 public class Animal {
 
-    private String animalName;
-    private int animalWeight = 12;
-    private String animalFood = "fish";
-    private int pawsCount = 4;
-    private int eyesCount = 2;
-    private int hoursSleepsPerDay = 7;
-    private int animalEnergy = 6;
-    private boolean isSleeping = false;
+    private String name;
+    private boolean isSleeping;
+    private int healthPoints = 4;
+    private int quantityOfLegs;
 
-    public Animal(String animalName) {
-        this.animalName = animalName;
+    public Animal() {
+    log.info("Object created");
     }
 
-    public Animal(int eyesCount, int animalWeight, String animalFood, int pawsCount, int hoursSleepsPerDay, int animalEnergy) {
-
-        this.eyesCount = eyesCount;
-        this.animalWeight = animalWeight;
-        this.animalFood = animalFood;
-        this.pawsCount = pawsCount;
-        this.hoursSleepsPerDay = hoursSleepsPerDay;
-        this.animalEnergy = animalEnergy;
-
+    public Animal(String name) {
+        this.name = name;
+        Animal.log.info("Object" + this.name + "created");
     }
 
-    public int getAnimalWeight() {
-        return animalWeight;
+    public Animal(String name, boolean isSleeping, int healthPoints, int quantityOfLegs) {
+        this.name = name;
+        this.isSleeping = isSleeping;
+        this.healthPoints = healthPoints;
+        this.quantityOfLegs = quantityOfLegs;
+        Animal.log.info("Object" + this.name + "created");
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        System.out.println("set user name > " + name);
+    }
+
+    public void status() {
+        System.out.printf("status >\n" +
+                "\tAnimal\t\t:\t%s\n" +
+                "\tIs sleeping\t:\t%s\t\n" +
+                "\tHealth\t\t:\t%s\t\n" +
+                "\tLegs\t\t:\t%s\n", name, isSleeping, healthPoints, quantityOfLegs);
+    }
+    public void feed() {
+        System.out.print("feed > ");
+        if (isSleeping) {
+            System.out.println("Can`t feed " + name + " is sleeping, awake first");
+        } else if (healthPoints < 4) {
+            healthPoints++;
+            System.out.println(name + " is fed, HP = " + this.healthPoints);
+        } else System.out.println(name + " is not hungry");
+    }
+    public void sleep() {
+        System.out.print("sleep > ");
+        if (!isSleeping) {
+            isSleeping = true;
+            System.out.println(name + " went sleep");
+        } else System.out.println(name + " is already sleeping");
+    }
+
+    public void awake() {
+        System.out.print("awake > ");
+        if (isSleeping) {
+            isSleeping = false;
+            System.out.println(name + " awakened");
+        } else System.out.println(name + " is not sleeping");
     }
 
     public void walk() {
-        if (animalEnergy == 0) {
-            System.out.println("Animal can't do any exercise! You need to feed it.");
-        } else {
-            animalEnergy--;
-            System.out.println("That was a good walk!");
-        }
+        checkHP("walk");
+    }
+
+    public void train() {
+        checkHP("train");
     }
 
     public void play() {
-        if (animalEnergy == 0) {
-            System.out.println("Animal can't do any exercise! You need to feed it.");
-        } else {
-            animalEnergy--;
-            System.out.println("I like playing with You!");
-        }
+        checkHP("play");
     }
 
-    public void training() {
-        if (animalEnergy == 0) {
-            System.out.println("Animal can't do any exercise! You need to feed it.");
-        } else {
-            animalEnergy--;
-            System.out.println("Trainings are good effect on me!");
-        }
+    private void checkHP(String actionPhrase) {
+        System.out.print(actionPhrase + " > ");
+        if (isSleeping) {
+            System.out.println(name + " is sleeping, awake first");
+        } else if (healthPoints > 0) {
+            healthPoints--;
+            System.out.println(name + " " + actionPhrase + "ing success " + healthPoints + " Health points left");
+        } else
+            System.out.println(name + " can`t go " + actionPhrase + "ing, too tired, please feed " + name);
     }
-
-    public void feed() {
-        if (animalEnergy == 6) {
-            System.out.println("Animal full of energy.");
-        } else {
-            animalEnergy = 6;
-            System.out.println("Yammy, yammy! Thanks for feeding me!");
-        }
-    }
-
 }
-
+//
+//    public Animal(String animalName) {
+//        this.animalName = animalName;
+//    }
+//
+//    public Animal(int eyesCount, int animalWeight, String animalFood, int pawsCount, int hoursSleepsPerDay, int animalEnergy) {
+//
+//        this.eyesCount = eyesCount;
+//        this.animalWeight = animalWeight;
+//        this.animalFood = animalFood;
+//        this.pawsCount = pawsCount;
+//        this.hoursSleepsPerDay = hoursSleepsPerDay;
+//        this.animalEnergy = animalEnergy;
+//
+//    }
+//
+//    public int getAnimalWeight() {
+//        return animalWeight;
+//    }
+//
+//    public void walk() {
+//        if (animalEnergy == 0) {
+//            System.out.println("Animal can't do any exercise! You need to feed it.");
+//        } else {
+//            animalEnergy--;
+//            System.out.println("That was a good walk!");
+//        }
+//    }
+//
+//    public void play() {
+//        if (animalEnergy == 0) {
+//            System.out.println("Animal can't do any exercise! You need to feed it.");
+//        } else {
+//            animalEnergy--;
+//            System.out.println("I like playing with You!");
+//        }
+//    }
+//
+//    public void training() {
+//        if (animalEnergy == 0) {
+//            System.out.println("Animal can't do any exercise! You need to feed it.");
+//        } else {
+//            animalEnergy--;
+//            System.out.println("Trainings are good effect on me!");
+//        }
+//    }
+//
+//    public void feed() {
+//        if (animalEnergy == 6) {
+//            System.out.println("Animal full of energy.");
+//        } else {
+//            animalEnergy = 6;
+//            System.out.println("Yammy, yammy! Thanks for feeding me!");
+//        }
+//    }
+//
+//}
+//
--- src/main/java/homework/AnimalExercises.java
+++ src/main/java/homework/AnimalExercises.java
@@ -3,13 +3,40 @@ package homework;
 public class AnimalExercises {
 
     public static void main(String[] args) {
-        Animal kitty = new Animal("Kitty");
 
-        kitty.walk();
-        kitty.training();
-        kitty.play();
-        kitty.getAnimalWeight();
-        System.out.println("Animal weight" + kitty.getAnimalWeight() + "kg");
+        Animal cat = new Animal("Cat");
+        Animal tiger = new Animal("Tiger");
+        Animal snake = new Animal("Snake");
+        Animal dog = new Animal("Dog");
+        Animal hippo = new Animal("Hippo");
+        Animal camel = new Animal("Camel");
+
+        cat.status();
+        cat.sleep();
+        cat.sleep();
+        cat.awake();
+        cat.awake();
+        cat.train();
+        cat.walk();
+        cat.play();
+        cat.train();
+        cat.train();
+        cat.feed();
+        cat.feed();
+        cat.feed();
+        cat.feed();
+        cat.feed();
+        cat.sleep();
+        cat.feed();
+        cat.sleep();
+        cat.play();
+
+        ObjectsHomework objHome = new ObjectsHomework();
+        objHome.test("test");
+    }
+
+    void test(String test) {
+        System.out.println("test");
     }
 
 }
